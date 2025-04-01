53760acbf2d751ab57fdac53486999bfd25661c6
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-04-01 12:41:05 -0300

    Signup back

--- server/src/main/java/com/example/bookit/Controllers/UserController.java
+++ server/src/main/java/com/example/bookit/Controllers/UserController.java
@@ -3,24 +3,41 @@ package com.example.bookit.Controllers;
 import com.example.bookit.Entities.User;
 import com.example.bookit.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")  // Permite que el frontend acceda al backend
 public class UserController {
+
     @Autowired
     private UserService userService;
 
     @PostMapping("/signup")
-    public User signup(@RequestBody User user){
-        return userService.registerUser(user.getUsername(), user.getPassword(), user.getEmail(), user.getFullName(), user.getBirthdate(), user.getInterests());
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        try {
+            User newUser = userService.registerUser(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getEmail(),
+                    user.getFullName(),
+                    user.getBirthdate(),
+                    user.getInterests()
+            );
+            return ResponseEntity.ok(newUser);  // Devuelve el usuario registrado
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());  // Devuelve el error si algo falla
+        }
     }
 
     @PostMapping("/login")
-    public User login(@RequestBody User user){
-        return userService.loginUser(user.getEmail(), user.getPassword());
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            User loggedInUser = userService.loginUser(user.getEmail(), user.getPassword());
+            return ResponseEntity.ok(loggedInUser);  // Devuelve el usuario si el login es exitoso
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body(e.getMessage());  // Devuelve error si el login falla
+        }
     }
 }

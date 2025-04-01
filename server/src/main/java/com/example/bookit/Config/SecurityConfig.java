4e61d96978b6d2e087eef08bbf79c07487824b5e
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-04-01 10:38:30 -0300

    cambios

--- server/src/main/java/com/example/bookit/Config/SecurityConfig.java
+++ server/src/main/java/com/example/bookit/Config/SecurityConfig.java
@@ -4,14 +4,5 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterchain securityFilterchain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-                .formLogin(httpform -> {
-                    httpform
-                            .loginPage( )
-                })
-    }
 }

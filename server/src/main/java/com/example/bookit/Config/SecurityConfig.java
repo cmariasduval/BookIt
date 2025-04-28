e7e08b96e1c56446d5e0890861a85978f136441f
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-04-28 18:35:25 -0300

    SecurityConfig changes

--- server/src/main/java/com/example/bookit/Config/SecurityConfig.java
+++ server/src/main/java/com/example/bookit/Config/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig {
 
 
 
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();

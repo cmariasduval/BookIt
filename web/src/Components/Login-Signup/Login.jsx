8b36586ae68ba5b905194fd9aadfb6157096ac72
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-07-28 21:28:32 -0300

    login con google funcionando

--- web/src/Components/Login-Signup/Login.jsx
+++ web/src/Components/Login-Signup/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ credential: credentialResponse.credential })
+                body: JSON.stringify({ idToken: credentialResponse.credential })
             });
 
             if (response.ok) {
